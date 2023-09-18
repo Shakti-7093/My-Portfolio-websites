cebfdce15ce2ce9bcfd29af069c00a43a210afd4
Shakti-7093 <145318489+Shakti-7093@users.noreply.github.com>
2023-09-18 16:23:15 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+function sum(x,y){
+    return x+y;
+}
+console.log('Extenal >',sum(1,10));
+const nameRef = document.getElementById('name');
+console.log(nameRef);
+const descriptionRef = document.getElementsByClassName('main');
+console.log(descriptionRef);
